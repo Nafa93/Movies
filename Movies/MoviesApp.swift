2e9f8381e67f030ba394d8506a28ba55c20e9aef
Nicolas Alejandro Fernandez Amorosino <nicolasfernandez93@gmail.com>
2024-09-03 17:49:44 -0300

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Nicolas Alejandro Fernandez Amorosino on 03/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
